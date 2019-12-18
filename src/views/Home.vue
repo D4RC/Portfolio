4e6df3d3d86483e8ea37abc6f853d61e5d18acfe
Daniel Rdz <darc3012@gmail.com>
2019-12-18 00:36:01 -0600

    feature: header

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,66 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <header>
+    <section id="home" class="hero is-fullheight">
+      <div class="hero-head">
+        <b-navbar transparent wrapper-class="container" type="is-dark" spaced>
+          <template slot="end">
+            <b-navbar-item href="#">Acerca</b-navbar-item>
+            <b-navbar-item href="#">Tecnologías</b-navbar-item>
+            <b-navbar-item href="#">Proyectos</b-navbar-item>
+            <b-navbar-item href="#">Contacto</b-navbar-item>
+          </template>
+        </b-navbar>
+      </div>
+
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h5 class="is-size-5 has-text-white">Hola, soy</h5>
+          <h1 class="title is-1 has-text-weight-medium has-text-white">Daniel Rodríguez</h1>
+          <h2 class="subtitle is-3 has-text-aqua">Ing. en Tecnologías de la Información</h2>
+
+          <div class="block">
+          <a class="mx-3">
+            <b-icon 
+              pack="fab"
+              icon="linkedin-in"
+              size="is-medium"
+              type="is-white"
+            ></b-icon>
+          </a>
+          <a class="mx-3">
+            <b-icon
+              pack="fab"
+              icon="github"
+              size="is-medium"
+              type="is-white"
+            ></b-icon>
+          </a>
+          <a class="mx-3">
+            <b-icon
+              pack="far"
+              icon="envelope"
+              size="is-medium"
+              type="is-white"
+            ></b-icon>
+          </a>
+          </div>
+          <b-button type="is-aqua" size="is-medium" class="has-text-white" outlined>Contáctame</b-button>
+        </div>
+      </div>
+
+    </section>
+  </header>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+<style>
+#home {
+  background: url("../assets/landscape.jpg");
+  background-size: cover;
 }
-</script>
+
+.mx-3 {
+  margin: 0 1rem 0 1rem;
+}
+
+
+</style>
